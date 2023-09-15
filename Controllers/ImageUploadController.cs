db4ca13bfda3de03cabe99a0f2b0ca3f11b95de8
Trinhf21522717 <21522717@gm.uit.edu.vn>
2023-09-15 23:51:07 +0700

    upgrade api

--- Controllers/ImageUploadController.cs
+++ Controllers/ImageUploadController.cs
@@ -19,105 +19,15 @@ namespace Advanced.Controllers
             _logger = logger;
             _hubContext = hubContext;
         }
-        /*
+
         [HttpPost]
-        public async Task<IActionResult> PostImage()
+        public async Task<IActionResult> PostImage([FromForm] string imageData, [FromForm] string encodeType)
         {
-            // Đọc dữ liệu từ phần thân yêu cầu dưới dạng luồng
-            using (Stream requestStream = Request.Body)
-            {
-                try
-                {
-                    byte[] buffer = new byte[1024]; // Buffer để đọc dữ liệu
-                    int bytesRead;
-                    using (MemoryStream memoryStream = new MemoryStream())
-                    {
-                        while ((bytesRead = await requestStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
-                        {
-                            memoryStream.Write(buffer, 0, bytesRead);
-                        }
-                        byte[] imageData = memoryStream.ToArray();
+            await _hubContext.Clients.All.SendAsync("ReceiveImage", imageData, encodeType);
 
-
-                        // Console.WriteLine(_hubContext.Clients.Count());
-
-                        await _hubContext.Clients.All.SendAsync("ReceiveImage", imageData);
-                        // _hubContext.SendImage()
-
-                        Console.WriteLine("Anh up roi ne");
-                        Console.WriteLine(imageData.Length);
-
-                        // Xử lý dữ liệu ảnh ở đây
-                        // imageData chứa dữ liệu ảnh dưới dạng byte[]
-                        // Ví dụ: Lưu trữ ảnh vào một tệp trên máy chủ
-                        // File.WriteAllBytes("path/to/save/image.jpg", imageData);
-
-                        return Ok(new { message = "Image uploaded" });
-                    }
-                }
-                catch (Exception ex)
-                {
-                    // Xử lý lỗi nếu cần
-                    return StatusCode(500, new { message = "Error processing the request" });
-                }
-            }
-        }
-        */
-
-        /*
-        [HttpPost]
-        public async Task<IActionResult> PostImage(ICollection<IFormFile> files)
-        {
-            Console.WriteLine($"Count {files.Count()}");
-            await _hubContext.Clients.All.SendAsync("ReceiveImage", new {imageData = files.FirstOrDefault()});
-            // _hubContext.SendImage()
-
-            Console.WriteLine("Anh up roi ne");
-
-            // Xử lý dữ liệu ảnh ở đây
-            // imageData chứa dữ liệu ảnh dưới dạng byte[]
-            // Ví dụ: Lưu trữ ảnh vào một tệp trên máy chủ
-            // File.WriteAllBytes("path/to/save/image.jpg", imageData);
+            Console.WriteLine("Got image");
 
             return Ok(new { message = "Image uploaded" });
         }
-        */
-
-        [HttpPost]
-        public async Task<IActionResult> PostImage([FromForm] string imageData)
-        {
-            await _hubContext.Clients.All.SendAsync("ReceiveImage", imageData);
-            // _hubContext.SendImage()
-
-            Console.WriteLine("Anh up roi ne");
-            Console.WriteLine(imageData);
-
-            // Xử lý dữ liệu ảnh ở đây
-            // imageData chứa dữ liệu ảnh dưới dạng byte[]
-            // Ví dụ: Lưu trữ ảnh vào một tệp trên máy chủ
-            // File.WriteAllBytes("path/to/save/image.jpg", imageData);
-
-            return Ok(new { message = "Image uploaded" });
-        }
-
-        /*
-        [HttpPost]
-        public async Task<IActionResult> PostImage([FromQuery] string Message)
-        {
-
-            await _hubContext.Clients.All.SendAsync("ReceiveMessage", Message);
-            // _hubContext.SendImage()
-
-            Console.WriteLine($"Message up roi ne {Message}");
-            // Console.WriteLine(imageData.Length);
-
-            // Xử lý dữ liệu ảnh ở đây
-            // imageData chứa dữ liệu ảnh dưới dạng byte[]
-            // Ví dụ: Lưu trữ ảnh vào một tệp trên máy chủ
-            // File.WriteAllBytes("path/to/save/image.jpg", imageData);
-
-            return Ok(new { message = "Image uploaded" });
-        }
-        */
     }
 }
