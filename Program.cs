6538e41ffbe214a48720bf06491cd487e7779b12
Trinhf21522717 <21522717@gm.uit.edu.vn>
2023-09-17 22:31:11 +0700

    add tls1.1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 using Microsoft.AspNetCore.SignalR;
 using Advanced.Models;
 
+using System.Net;
+
+// Bật hỗ trợ cho TLS 1.1 và TLS 1.2
+ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
